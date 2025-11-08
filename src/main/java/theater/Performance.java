d2b0b0ae732a286fefd8a5bf9433d99b5b5802a8
bsu <cheb.su@mil.utoronto.ca>
2025-11-08 15:50:43 -0500

    Style changes to all file in main

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,25 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
+ * @returns playID and audience
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
         this.audience = audience;
     }
+
+    public String getID() {
+        return this.playID;
+    }
+
+    public int getAudience() {
+        return this.audience;
+    }
+
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * THis is a player class that does not change.
+ * @returns name and type
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
     }
 }
